0721c405fe8d382fde2ee36206ded740d5dd7e2d
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-24 15:55:59 +0300

    ForEach on views

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -1,12 +1,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
+    let students = ["Hary", "Cosmo", "John"]
+    @State private var selectedStudent = "Hary"
     
     var body: some View {
-        Form {
-            TextField("Enter your name", text: $name)
-            Text("Your name is: \(name)")
+        NavigationStack {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+            .navigationTitle("Select a student")
         }
     }
 }
