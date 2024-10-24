5f163a5f39787a9a3a1266d3ee36c8b6912a9b5e
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-24 15:25:53 +0300

    Navigation Stack

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -2,18 +2,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello World!")
-                Text("Hello World!")
-                Text("Hello World!")
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Hello World!")
+                    Text("Hello World!")
+                    Text("Hello World!")
+                }
+                
+                Section {
+                    Text("Hello World 2!")
+                    Text("Hello World 2!")
+                    Text("Hello World 2!")
+                }
             }
-            
-            Section {
-                Text("Hello World 2!")
-                Text("Hello World 2!")
-                Text("Hello World 2!")
-            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.large)
         }
     }
 }
