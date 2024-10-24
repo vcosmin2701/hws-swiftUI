6651ccd02f06d417494750186e83a79c5899d695
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-24 10:39:55 +0300

    Initial project configuration

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vladut Cosmin on 24.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
