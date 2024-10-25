68593184e3f24648fe61fa08b8c0a2f91db55662
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-25 10:30:59 +0300

    Initial files for the project

---
+++ UnitConversion/UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Vladut Cosmin on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
