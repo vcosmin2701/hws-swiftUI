01dfbeba6122ace97d58d41fc228d61ff4ad03d3
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-25 10:58:04 +0300

    Picker for selecting the input and output type

--- UnitConversion/UnitConversion/ContentView.swift
+++ UnitConversion/UnitConversion/ContentView.swift
@@ -1,21 +1,47 @@
-//
-//  ContentView.swift
-//  UnitConversion
-//
-//  Created by Vladut Cosmin on 25.10.2024.
-//
-
 import SwiftUI
 
+enum TemperatureType {
+    case celsius, fahrenheit, kelvin
+}
+
 struct ContentView: View {
+    @State private var inputType: TemperatureType = .celsius
+    @State private var outputType: TemperatureType = .fahrenheit
+    @State private var inputValue = 0.0
+    @State private var outputValue = 0.0
+    
+    let units: [TemperatureType] = [.celsius, .fahrenheit, .kelvin]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Input type") {
+                    Picker("From", selection: $inputType) {
+                        ForEach(units,  id: \.self) { unit in
+                            Text("\(unit)")
+                        }
+                    }
+                }
+                
+                Section("Output type") {
+                    Picker("To", selection: $outputType) {
+                        ForEach(units, id: \.self) { unit in
+                            Text("\(unit)")
+                        }
+                    }
+                }
+                
+                Section("Initial value") {
+                    Text("Value: ")
+                }
+                
+                Section("Conversion value") {
+                    Text("Value:")
+                }
+            }
+            .navigationTitle("Conversion App")
         }
-        .padding()
     }
 }
 
--- UnitConversion/UnitConversion/UnitConversionApp.swift
+++ UnitConversion/UnitConversion/UnitConversionApp.swift
@@ -1,10 +1,3 @@
-//
-//  UnitConversionApp.swift
-//  UnitConversion
-//
-//  Created by Vladut Cosmin on 25.10.2024.
-//
-
 import SwiftUI
 
 @main
