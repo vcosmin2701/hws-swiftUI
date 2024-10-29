0f2e5c83c70851694d30dec4246df8ff1e8d078b
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-29 09:16:44 +0200

    Colors and Frames

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -2,25 +2,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Spacer()
-            
-            Text("Hello, this is a text")
-            Text("This is another text")
-            
-            Spacer()
-        }
-        
-        HStack(spacing: 20) {
-            Text("Apple")
-            Text("Team")
-        }
-        
         ZStack {
-            Text("⛅️")
-                .font(.system(size: 30))
-            Text("🦊")
+            // Color. : .primary, .secondary -> semantic colors
+            // Custom color: Color(red: 1, green: 0.8, blue: 0)
+            
+            // Color.yellow // will be treated as a view, whole content will be red
+                //.frame(width: 200, height: 200)
+                // .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
+            
+            Text("Your content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial) // vibrancy
+            
         }
+        .ignoresSafeArea()
     }
 }
 
