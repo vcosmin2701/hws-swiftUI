2e62855b0b873710c08b55f287b1df77c7e6cdd8
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-29 09:57:40 +0200

    Alerts

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -1,35 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack {
-            Button("Button1") { }
-                .buttonStyle(.bordered)
-            
-            Button("Button 2", role: .destructive) { }
-                .buttonStyle(.bordered)
-            
-            Button("Button 3") { }
-                .buttonStyle(.borderedProminent)
-            
-            Button("Button 4", role: .destructive) { }
-                .buttonStyle(.borderedProminent)
-                .tint(.indigo)
-            
-            Button("Edit", systemImage: "pencil.circle") {
-                print("Button was pressed!")
-            }
-            
-            Button {
-                print("Button was tapped")
-            } label: {
-                Label("Edit 2", systemImage: "pencil") // better than HStack in this case
-            }
-            
-            // Image(decorative: "singapore") -> decorative for screen readers
-            Image(systemName: "pencil.circle")
-                .foregroundStyle(.red)
-                .font(.largeTitle)
+        Button("Show alert"){
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) { }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Please read this.")
         }
     }
 }
