6885dcf96560fcd5aeab2f71472303ea28a071ed
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-29 10:36:37 +0200

    Simple game logic
    The user will tap on the flag and will find out if is correct or not, and then it goes to another round of guessing flags

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -1,9 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
     var body: some View {
         ZStack {
@@ -18,13 +21,31 @@ struct ContentView: View {
                 
                 ForEach(0..<3) { number in
                     Button() {
-                        // button tapped
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                     }
                 }
             }
+        }.alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
         }
+    }
+    
+    func flagTapped(_ number: Int){
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0..<3)
     }
 }
 
