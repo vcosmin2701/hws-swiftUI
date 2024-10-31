7aff30918fc38ac08a668d88555d7261edad9ae5
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-31 16:48:37 +0200

    Choices UI implementation for the game

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -11,12 +11,12 @@ struct ContentView: View {
                 Text("Computer")
             }
             
-            Image(systemName: choices[0])
+            GameChoice(choices[0])
             
-            HStack(spacing: 20){
-                Image(systemName: choices[0])
-                Image(systemName: choices[1])
-                Image(systemName: choices[2])
+            HStack(spacing: 10){
+                GameChoice(choices[0])
+                GameChoice(choices[1])
+                GameChoice(choices[2])
             }
         }
     }
@@ -25,3 +25,40 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+struct GameChoice: View {
+    var text: String
+    
+    private var gradient = Gradient(colors: [
+        Color(.blue),
+        Color(.indigo)
+    ])
+    
+    init(_ text: String) {
+        self.text = text
+    }
+    
+    var body: some View {
+        Button {
+            print("tapped")
+        } label: {
+            ZStack {
+                Circle()
+                    .fill(
+                        RadialGradient(
+                            gradient: gradient,
+                            center: .center,
+                            startRadius: 1,
+                            endRadius: 100
+                        )
+                    )
+                    .frame(width: 100, height: 100)
+                Image(systemName: text)
+            }
+        }
+        .font(.system(size: 30))
+        .foregroundStyle(.white)
+    }
+}
+
