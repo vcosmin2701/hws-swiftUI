e718b46cadba479c6cfd5031a2e391b9e42b0775
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-31 15:45:55 +0200

    Initial stage for RPS Challenge
    Just simple UI elements in order to have a starting point

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Vladut Cosmin on 31.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
