da2eafc2cddc1377aef9a27cc0f73fbf7dcc74b2
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-31 20:35:52 +0200

    Refactoring and game logic implemented

--- RockPaperScissors/RockPaperScissors/GameChoice.swift
+++ RockPaperScissors/RockPaperScissors/GameChoice.swift
@@ -1,50 +1,35 @@
 import SwiftUI
 
 struct GameChoice: View {
-    private var choices = ["newspaper", "mountain.2", "scissors"]
-    var currentChoice: ChoiceType
+    var currentChoice: String
     
     private var gradient = Gradient(colors: [
         Color(.blue),
         Color(.indigo)
     ])
     
-    private var imageName: String {
-        switch currentChoice {
-        case .rock:
-            "🪨"
-        case .paper:
-            "📄"
-        case .scissors:
-            "✂️"
-        }
-    }
-    
-    init(_ choice: ChoiceType){
+    init(_ choice: String){
         self.currentChoice = choice
     }
     
     var body: some View {
-        Button {
-            print("tapped")
-        } label: {
-            ZStack {
-                Circle()
-                    .fill(
-                        RadialGradient(
-                            gradient: gradient,
-                            center: .center,
-                            startRadius: 1,
-                            endRadius: 100
-                        )
+        ZStack {
+            Circle()
+                .fill(
+                    RadialGradient(
+                        gradient: gradient,
+                        center: .center,
+                        startRadius: 1,
+                        endRadius: 100
                     )
-                    .frame(width: 100, height: 100)
-                
-                Text(imageName)
-                    .font(.largeTitle)
-            }
+                )
+                .frame(width: 100, height: 100)
+            
+            Text(currentChoice)
+                .font(.largeTitle)
         }
         .font(.system(size: 30))
         .foregroundStyle(.white)
     }
 }
+
