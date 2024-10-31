cf36468d2b82608ce1ccb043ac07841507f0ea52
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-31 10:19:58 +0200

    Custom containers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -1,48 +1,30 @@
 import SwiftUI
 
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundStyle(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(.rect(cornerRadius: 10))
-    }
-}
-
-struct Watermark: ViewModifier {
-    var text: String
+//Generics
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content // content closure
     
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            
-            Text(text)
-                .font(.caption)
-                .foregroundStyle(.white)
-                .padding(5)
-                .background(.black)
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
         }
-    }
-    
-}
-
-extension View {
-    func titleStyle() -> some View {
-        modifier(Title())
-    }
-    
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .titleStyle()
-            .watermarked(with: "AppleTeam")
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
     }
 }
 
