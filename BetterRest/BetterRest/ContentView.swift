7e6540ab4aa4f77df92b33fb5918d90ad3096602
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-01 10:11:35 +0200

    Displaying dates

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -5,6 +5,9 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     
     var body: some View {
+        Text(Date.now, format: .dateTime.day().month().year())
+        Text(Date.now.formatted(date: .long, time: .shortened))
+        
         Stepper(
             "\(sleepAmount.formatted()) hours",
             value: $sleepAmount,
@@ -17,8 +20,14 @@ struct ContentView: View {
     }
     
     func exampleDates() {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//    
+//        let date = Calendar.current.date(from: components) ?? .now
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
