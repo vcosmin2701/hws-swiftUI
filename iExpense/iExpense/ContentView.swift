6dd4a0f4713c579113d0aeeb7425ab1d9334137b
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-05 13:13:13 +0200

    [project-7] - iExpense project - Some changes are that I used @Observable but adapted to iOS 16+ instead of 17

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -1,82 +1,41 @@
 import SwiftUI
-import Observation
-
-// @Observable
-class User: ObservableObject{
-    @Published var firstName = "Bruno"
-    @Published var lastName = "Bruce"
-    
-    func getFullName() -> String{
-        "\(firstName) \(lastName)"
-    }
-}
-
-struct Freak: Codable {
-    let name: String
-    let team: String
-}
 
 struct ContentView: View {
-    @StateObject private var user = User()
-    @State private var showingSheet = false
+    @StateObject private var expenses = Expenses()
+    //@State private var expenses = Expenses() iOS 17+
+    @State private var showingAddExpense = false
     
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
-    
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
-    // @AppStorage("tapCount") private var tapCount = 0
-    
-    @State private var freak = Freak(name: "Cosmo", team: "Apple Team")
-    
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Your name is: \(user.getFullName())")
-                
-                TextField("name", text: $user.firstName)
-                
-                Button("Show Sheet"){
-                    showingSheet.toggle()
-                }.sheet(isPresented: $showingSheet) {
-                    SecondView(name: "Cosmo")
-                }
-                
-                Button("Save freak to DB") {
-                    let encoder = JSONEncoder()
-                    
-                    if let data = try? encoder.encode(freak) {
-                        UserDefaults.standard.set(data, forKey: "FreakData")
-                    }
-                }
-                
-                VStack {
-                    List {
-                        ForEach(numbers, id: \.self) {
-                            Text("Row \($0)")
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
                         }
-                        .onDelete(perform: removeRows)
-                    }
-                    
-                    Button("Add Number") {
-                        numbers.append(currentNumber)
-                        currentNumber += 1
+                        
+                        Spacer()
+                        
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     }
                 }
-                
-                VStack {
-                    Button("Tap count: \(tapCount)") {
-                        tapCount += 1
-                        UserDefaults.standard.set(tapCount, forKey: "Tap")
-                    }
-                }
+                .onDelete(perform: removeItems)
             }
+            .navigationTitle("iExpense")
             .toolbar {
-                EditButton()
+                Button("Add Expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
             }
+        }
+        .sheet(isPresented: $showingAddExpense){
+            AddView(expenses: expenses)
         }
     }
 }
