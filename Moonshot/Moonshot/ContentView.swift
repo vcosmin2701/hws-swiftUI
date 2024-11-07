c6cc2f1e45f27c88cdf197c753e3d71c58a48c21
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-07 10:01:55 +0200

    NavigationLink example

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -1,14 +1,40 @@
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        ScrollView(.horizontal) {
+//            LazyHStack(spacing: 10) {
+//                ForEach(0..<100) {
+//                    CustomText(text: "Item \($0)")
+//                        .font(.title)
+//                }
+//            }
+//            .frame(maxWidth: .infinity)
+//        }
+        
+        NavigationStack{
+            List(0..<100) { row in
+                NavigationLink("Row \(row)"){
+                    Text("Detail \(row)")
+                }
+            }
+            .navigationTitle("SwiftUI")
         }
-        .padding()
+        
+        
     }
 }
 
