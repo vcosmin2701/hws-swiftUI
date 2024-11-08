03af8b6722c36a783fc8e9c3b1e9b41700135c79
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-08 10:19:08 +0200

    [project-8] - JSON parsing and Generics

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -1,83 +1,55 @@
 import SwiftUI
 
-struct CustomText: View {
-    let text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(text: String) {
-        print("Creating a new CustomText")
-        self.text = text
-    }
-}
-
-// conforms Codable
-struct Company: Codable {
-    let company_name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let city: String
-    let country: String
-}
-
 struct ContentView: View {
-    let layout = [
-//        GridItem(.fixed(80)),
-//        GridItem(.fixed(80)),
-//        GridItem(.fixed(80))
-        GridItem(.adaptive(minimum: 80, maximum: 120))
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
     ]
+    
     var body: some View {
-        //        ScrollView(.horizontal) {
-        //            LazyHStack(spacing: 10) {
-        //                ForEach(0..<100) {
-        //                    CustomText(text: "Item \($0)")
-        //                        .font(.title)
-        //                }
-        //            }
-        //            .frame(maxWidth: .infinity)
-        //        }
-        
-        NavigationStack{
-            List(0..<5) { row in
-                NavigationLink("Row \(row)"){
-                    Text("Detail \(row)")
-                }
-            }
-            .navigationTitle("SwiftUI")
-            
-            Button("DECODE JSON") {
-                let input = """
-                                {
-                                    "company_name": "Agile Freaks",
-                                    "address": {
-                                        "city": "Sibiu",
-                                        "country": "Romania"
-                                    }
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.gray)
                                 }
-                                """
-                
-                let data = Data(input.utf8)
-                let decoder = JSONDecoder()
-                
-                if let company = try? decoder.decode(Company.self, from: data) {
-                    print(company.company_name)
-                } else {
-                    print("Failed to decode JSON")
-                }
-            }
-            
-            ScrollView(){
-                LazyVGrid(columns: layout) {
-                    ForEach(0..<100) {
-                        Text("Item \($0)")
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                            
+                        }
                     }
                 }
+                .padding([.horizontal, .bottom])
             }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
     }
 }
