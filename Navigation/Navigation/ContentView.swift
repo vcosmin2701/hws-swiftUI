c0b3c16661b9045b9f05fe3cc17c90642d658f27
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-12 11:55:02 +0200

    How to save NavigationPath using Codable

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -1,24 +1,44 @@
 import SwiftUI
 
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath){
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data.")
+        }
+    }
+}
+
 struct DetailView: View {
     var number: Int
-    @Binding var path: [Int]
-    
-    // or
-    // @Binding var path: NavigationPath
     
     var body: some View {
         NavigationLink("Go to random number", value: Int.random(in: 1...1000))
             .navigationTitle("Number: \(number)")
-            .toolbar {
-                Button("Home") {
-                    path.removeAll()
-                    
-                    // or
-                    
-                    // path = NavigationPath()
-                }
-            }
     }
 }
 
@@ -26,17 +46,17 @@ struct ContentView: View {
     // in this case if we want to go to the
     // very first view, we can removeAll()
     // because is an array of ints
-    @State private var path = [Int]()
+    // @State private var path = [Int]()
     
     // or
     
-    // @State private var path = NavigationPath()
+    @State private var pathStore = PathStore()
     
     var body: some View {
-        NavigationStack(path: $path){
-            DetailView(number: 0, path: $path)
+        NavigationStack(path: $pathStore.path){
+            DetailView(number: 0)
                 .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $path)
+                    DetailView(number: i)
                 }
         }
     }
