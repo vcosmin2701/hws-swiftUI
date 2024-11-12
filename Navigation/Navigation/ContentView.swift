fe9d0cd6a6a0d26ba4775f4c5205c4f28eea4cc2
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-12 12:12:06 +0200

    Toolbar customization

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -1,63 +1,33 @@
 import SwiftUI
 
-@Observable
-class PathStore {
-    var path: NavigationPath {
-        didSet {
-            save()
-        }
-    }
-    
-    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
-    
-    init() {
-        if let data = try? Data(contentsOf: savePath){
-            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
-                path = NavigationPath(decoded)
-                return
-            }
-        }
-        
-        path = NavigationPath()
-    }
-    
-    func save() {
-        guard let representation = path.codable else { return }
-        
-        do {
-            let data = try JSONEncoder().encode(representation)
-            try data.write(to: savePath)
-        } catch {
-            print("Failed to save navigation data.")
-        }
-    }
-}
-
-struct DetailView: View {
-    var number: Int
-    
-    var body: some View {
-        NavigationLink("Go to random number", value: Int.random(in: 1...1000))
-            .navigationTitle("Number: \(number)")
-    }
-}
-
 struct ContentView: View {
-    // in this case if we want to go to the
-    // very first view, we can removeAll()
-    // because is an array of ints
-    // @State private var path = [Int]()
-    
-    // or
-    
-    @State private var pathStore = PathStore()
     
     var body: some View {
-        NavigationStack(path: $pathStore.path){
-            DetailView(number: 0)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i)
+        NavigationStack {
+            List(0..<100) { i in
+                Text("Row \(i)")
+            }
+            .navigationTitle("Title goes here")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.blue)
+            .toolbarColorScheme(.dark)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Tap me") {
+                        // something goes here
+                    }
                 }
+                
+                ToolbarItemGroup(placement: .destructiveAction){
+                    Button("1") {
+                        //
+                    }
+                    
+                    Button("2") {
+                        //
+                    }
+                }
+            }
         }
     }
 }
