319f68766318cd3676622a951f3dafbf768e3ed3
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-12 10:55:17 +0200

    Usage of NavigationDestination
    A good practice is to separate the destination from the value
    In the Navigation link, we need to insert a value
    That value must to conform to hashable
    Built-in types from Swift, are already hashable
    If we create our own data type, we need to make it conform Hashable

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -1,28 +1,24 @@
 import SwiftUI
 
+struct Student: Hashable {
+    var id = UUID()
+    var name: String
+    var age: Int
+}
+
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            List(1..<1000){ i in
-                NavigationLink("Tap Me") {
-                    DetailView(number: i)
-                }
+            List(0..<100) { i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self) { selection in
+                    Text("You selected \(selection)")
+            }
+            .navigationDestination(for: Student.self) { student in
+                Text("You selected \(student.name)")
             }
         }
-    }
-}
-
-
-struct DetailView: View {
-    var number: Int
-    
-    var body: some View {
-        Text("Detail View \(number)")
-    }
-    
-    init(number: Int) {
-        self.number = number
-        print("Creating detail view \(number)")
     }
 }
 
