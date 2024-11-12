ac42637e4a070ea8e9217590c76cefc2b89082da
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-12 11:05:28 +0200

    Simple programmatic navigation in SwiftUI

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -1,22 +1,25 @@
 import SwiftUI
 
-struct Student: Hashable {
-    var id = UUID()
-    var name: String
-    var age: Int
-}
-
 struct ContentView: View {
+    @State private var path = [Int]()
+    
     var body: some View {
-        NavigationStack {
-            List(0..<100) { i in
-                NavigationLink("Select \(i)", value: i)
+        NavigationStack(path: $path){
+            VStack {
+                Button("Show 32") {
+                    path = [32]
+                }
+                
+                Button("Show 64") {
+                    path.append(64)
+                }
+                
+                Button("Show 32 and then 64"){
+                    path = [32, 64]
+                }
             }
             .navigationDestination(for: Int.self) { selection in
-                    Text("You selected \(selection)")
-            }
-            .navigationDestination(for: Student.self) { student in
-                Text("You selected \(student.name)")
+                Text("You selected \(selection)")
             }
         }
     }
