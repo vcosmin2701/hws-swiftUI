c788f19c847032969fd979a75fafde088a7b94e3
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-12 11:21:08 +0200

    Example of NavigationPath

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -1,25 +1,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var path = [Int]()
+    
+    // navigation path = type eraser, is not
+    // exposing what type the element inside is
+    @State private var path = NavigationPath()
     
     var body: some View {
         NavigationStack(path: $path){
-            VStack {
-                Button("Show 32") {
-                    path = [32]
+            List {
+                ForEach(0..<5) { i in
+                    NavigationLink("Select Number: \(i)", value: i)
                 }
                 
-                Button("Show 64") {
-                    path.append(64)
+                ForEach(0..<5) { i in
+                    NavigationLink("Select String \(i)", value: String(i))
+                }
+            }
+            .toolbar {
+                Button("Push 556") {
+                    path.append(556)
                 }
                 
-                Button("Show 32 and then 64"){
-                    path = [32, 64]
+                Button("Push Hello") {
+                    path.append("Hello")
                 }
             }
             .navigationDestination(for: Int.self) { selection in
-                Text("You selected \(selection)")
+                Text("You selected the number: \(selection)")
+            }
+            .navigationDestination(for: String.self) { selection in
+                Text("You selected the string \(selection)")
             }
         }
     }
