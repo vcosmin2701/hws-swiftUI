e2ad1a3add21a377dd38628f0bde3ccd87e9a960
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-15 10:35:33 +0200

    [part-3] - URLSession

--- CupcakeCorner/CupcakeCorner/AddressView.swift
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -1,17 +1,24 @@
-//
-//  AddressView.swift
-//  CupcakeCorner
-//
-//  Created by Vladut Cosmin on 14.11.2024.
-//
-
 import SwiftUI
 
 struct AddressView: View {
-    var order: Order
+    @Bindable var order: Order
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink("Check out") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
     }
 }
 
--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -1,7 +1,19 @@
 import Foundation
 
 @Observable
-class Order {
+class Order: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFroasting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddres"
+        case _zip = "zip"
+    }
+    
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
     var type = 0
@@ -17,4 +29,32 @@ class Order {
     }
     var extraFroasting = false
     var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        
+        return true
+    }
+    
+    var cost: Decimal {
+        var cost = Decimal(quantity) * 2
+        cost += Decimal(type) / 2
+        
+        if extraFroasting {
+            cost += Decimal(quantity)
+        }
+        
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+        
+        return cost
+    }
 }
